6fe4db30772c4dd867a0f00a664f90cb0ee0d89c
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2022-06-25 14:23:41 +0100

    Exercice 7 : DVDStore : Valoriser une propriété de type File

--- src/main/java/com/zayzou/dvdstore/repository/FileMovieRepository.java
+++ src/main/java/com/zayzou/dvdstore/repository/FileMovieRepository.java
@@ -2,15 +2,26 @@ package com.zayzou.dvdstore.repository;
 
 import com.zayzou.dvdstore.entity.Movie;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class FileMovieRepository implements  MovieRepositoryInterface {
 
-public void add(Movie movie){
+    private File file;
+
+    public File getFile() {
+        return file;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
+
+    public void add(Movie movie){
     FileWriter writer;
     try{
-        writer=new FileWriter("./movies.txt",true);
+        writer=new FileWriter(file,true);
         writer.write(movie.getTitle()+";"+ movie.getGenre()+"\n");
         writer.close();
     }
